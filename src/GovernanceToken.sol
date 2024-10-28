1c54e7a721366b4f2d456eaf340b47dc65ab0bd6
maxsakhno <contactsakhno@gmail.com>
2024-10-28 18:21:17 +0100

    update token name

--- src/GovernanceToken.sol
+++ src/GovernanceToken.sol
@@ -6,8 +6,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit, Nonces} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract MyToken is ERC20, ERC20Permit, ERC20Votes {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("GovernanceToken", "GTK") ERC20Permit("GovernanceToken") {}
 
     // The following functions are overrides required by Solidity.
 
